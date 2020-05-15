1a958fe660f181d90eb2302d1b8484d08cc3dc16
Javier Fernandes <javier.fernandes@gmail.com>
2020-05-15 18:35:17 -0300

    fixing table format

--- createReport.test.js
+++ createReport.test.js
@@ -1,19 +1,20 @@
-const { fileReport } = require('./createReport')
+const { assoc } = require('ramda')
+const createReport = require('./createReport')
+const { fileReport } = createReport
+
+const file = {
+  path: 'index.test.js',
+  tests: [
+    { test: 'testA', base: 1244, branch: 1244, delta: 0, deltaPercentage: 0.00 },
+    { test: 'testB', base: 10, branch: 20, delta: 10, deltaPercentage: 100.00 },
+    { test: 'testC', base: 20 },
+    { test: 'testD', branch: 40 },
+  ]
+}
 
 test('fileReport', () => {
-
-  const file = {
-    path: 'index.test.js',
-    tests: [
-      { test: 'testA', base: 1244, branch: 1244, delta: 0, deltaPercentage: 0.00 },
-      { test: 'testB', base: 10, branch: 20, delta: 10, deltaPercentage: 100.00 },
-      { test: 'testC', base: 20 },
-      { test: 'testD', branch: 40 },
-    ]
-  }
   expect(fileReport(file)).toEqual(
-`
-File: \`index.test.js\`
+`File: \`index.test.js\`
 
 | test | previous time (ms) | current time (ms) | delta (ms) | delta (%) |
 | ---- |          ---: |         ---: |        ---: |      ---: |
@@ -23,4 +24,34 @@ File: \`index.test.js\`
 | testD | - | 40 | - | - |
 `)
 
+})
+
+test('createReport', () => {
+  expect(createReport(undefined, [
+    file,
+    assoc('path', 'anotherFile.test.js')(file)
+  ])).toEqual(
+`
+# Test execution times differences 
+
+File: \`index.test.js\`
+
+| test | previous time (ms) | current time (ms) | delta (ms) | delta (%) |
+| ---- |          ---: |         ---: |        ---: |      ---: |
+| testA | 1244 | 1244 | 0 | 0.00% |
+| testB | 10 | 20 | 10 | 100.00% |
+| testC | 20 | - | - | - |
+| testD | - | 40 | - | - |
+
+File: \`anotherFile.test.js\`
+
+| test | previous time (ms) | current time (ms) | delta (ms) | delta (%) |
+| ---- |          ---: |         ---: |        ---: |      ---: |
+| testA | 1244 | 1244 | 0 | 0.00% |
+| testB | 10 | 20 | 10 | 100.00% |
+| testC | 20 | - | - | - |
+| testD | - | 40 | - | - |
+
+`
+  )
 })
--- dist/index.js
+++ dist/index.js
@@ -11572,17 +11572,16 @@ const createReport = (threshold, diffs) =>
 `
 # Test execution times differences ${threshold ? `(dt >= ${threshold}%)` : ''}
 
-  ${diffs.length > 0 ? diffs.map(fileReport) : (threshold ? 'No significant changes' : 'No changed') }
+${diffs.length > 0 ? diffs.map(fileReport).join('\n') : (threshold ? 'No significant changes' : 'No changed') }
 `
 
 const fileReport = ({ path, tests }) =>
-`
-File: \`${path}\`
+`File: \`${path}\`
+
 ${createTable(tests)}`
 
 const createTable = tests => 
-`
-| test | previous time (ms) | current time (ms) | delta (ms) | delta (%) |
+`| test | previous time (ms) | current time (ms) | delta (ms) | delta (%) |
 | ---- |          ---: |         ---: |        ---: |      ---: |
 ${
   tests.map(({ test, base, branch, delta, deltaPercentage }) => `| ${test} | ${value(base)} | ${value(branch)} | ${value(delta)} | ${deltaPercentage !== undefined ? deltaPercentage.toFixed(2) + '%' : '-'} |`)
